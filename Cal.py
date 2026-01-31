8e37cf1ae806343c81cc0f911ac2c469a2ad32ad
ANJIMARAJ25 <anjimarajvjec@gmail.com>
2026-01-31 09:25:10 +0530

    Create Cal.py

---
+++ Cal.py
@@ -0,0 +1,23 @@
+// calculator.c
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    printf("Enter operator (+ - * /): ");
+    scanf(" %c", &op);
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    switch(op) {
+        case '+': printf("Result = %d", a+b); break;
+        case '-': printf("Result = %d", a-b); break;
+        case '*': printf("Result = %d", a*b); break;
+        case '/': printf("Result = %.2f", (float)a/b); break;
+        default: printf("Invalid operator");
+    }
+
+    return 0;
+}
